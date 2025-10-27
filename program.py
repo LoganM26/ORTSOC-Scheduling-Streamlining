0667e45b6250bdbf34ceae064b6e3c76aac45f91
edaon8 <115111183+edaon8@users.noreply.github.com>
2025-10-27 12:16:05 -0700

    Initial script setup

--- program.py
+++ program.py
@@ -1,5 +1,44 @@
 #!/bin/env python
+import pandas as pd
+import uuid
+from datetime import datetime, timedelta, timezone
+from zoneinfo import ZoneInfo
 
-def Main():
+PST = ZoneInfo("America/Los_Angeles")
+
+# Formats to ICS datetime
+def createDatetime(date, time):
+    dt = datetime.combine(date, time, PST)
+    return dt.strftime("%Y%m%dT%H%M%S")
+
+# Returns an ICS VEVENT string
+"""
+Parameters:
+        uid (str): Unique event ID.
+        dtstamp (str): DTSTAMP in ICS format (YYYYMMDDTHHMMSSZ or TZ-aware).
+        dtstart (str): DTSTART in ICS format.
+        dtend (str): DTEND in ICS format.
+        summary (str): Event title.
+        description (str): Event description.
+"""
+def createEvent(uid, dtstamp, dtstart, dtend, summary, desc):
+    event = f"""BEGIN:VEVENT
+UID:{uid}
+DTSTAMP:{dtstamp}
+DTSTART;TZID={PST}:{dtstart}
+DTEND;TZID={PST}:{dtend}
+SUMMARY:{summary}
+DESCRIPTION:{desc}
+END:VEVENT
+"""
+    
+    return event
+
+
+def main():
     print("Hello World!")
-Main()
+    inputFile = "SOC Fall Schedule.xlsx"
+    outputFile = "master.ics"
+
+if __name__ == "__main__":
+    main()
